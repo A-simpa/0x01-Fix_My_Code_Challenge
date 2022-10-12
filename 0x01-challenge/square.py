4ac014ee5a3b64468fba5b0ce87ce1a8a6cfd84c
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-12 05:04:06 +0000

    lets see

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 
+class Square():
 
-class square():
+    width = 0
+    height = 0
 
-    size = 0
-
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) == 2 and \
+         (list(kwargs.values())[0] == list(kwargs.values())[1]):
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        elif len(args) == 2 and (args[0] == args[1]):
+            self.width = a[0]
+            self.height = a[1]
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.size ** 2
+        return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.size * 4)
+    def perimeter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.size, self.size)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
--- square.py
+++ square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 
-class square():
-    
+class Square():
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if len(kwargs) == 2 and \
+         (list(kwargs.values())[0] == list(kwargs.values())[1]):
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        elif len(args) == 2 and (args[0] == args[1]):
+            self.width = a[0]
+            self.height = a[1]
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
